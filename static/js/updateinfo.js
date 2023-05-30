b054bbfff43668896382b16671ac854454fa4bc2
penguin234 <56291718+penguin234@users.noreply.github.com>
2023-05-30 18:52:39 +0900

    Change radians to degress

--- static/js/updateinfo.js
+++ static/js/updateinfo.js
@@ -1,5 +1,6 @@
 function UpdateInfo(angle, distance) {
     let view = document.getElementById('arrowviewbox');
+    angle = angle * Math.PI / 180;
     view.innerHTML = DrawArrow(RotatePoints(pointsOrigin, angle));
 
     document.getElementById('distance').innerText = String(distance);
